c88401d5e38e7af653010faebc396f784c18aa3c
Carlos Sanchez <csanchez@maestrodev.com>
2013-10-23 12:08:15 +0200

    Anything using yum needs the proxy configured

--- definitions/CentOS-6.4-x86_64-minimal/cleanup.sh
+++ definitions/CentOS-6.4-x86_64-minimal/cleanup.sh
@@ -1,3 +1,8 @@
+if [ -f /home/veewee/.veewee_params ]
+then
+  . /home/veewee/.veewee_params
+fi
+
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
